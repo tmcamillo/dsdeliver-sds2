752a5459ce471a406a0f4f80aa1957025035e8c9
Tatiane Mendes Camillo <tatiane@Alexs-MacBook-Pro.local>
2021-01-08 20:06:02 -0300

    footer add

--- front-web/src/Home/Index.tsx
+++ front-web/src/Home/Index.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './styles.css';
-import { ReactComponent as MainImage} from './main.svg';
+import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer/Index';
 
 function Home() {
     return (
-        <nav className="home-container">
-            <div className="home-content">
-                <div className="home-action"> 
-                    <h1 className="home-title">Faça seu pedido<br/>que entregamos<br/>pra você!!!</h1>
-                    <h3 className="home-subtitle">
-                        Escolha o seu pedido e em poucos minutos<br/>levaremoss na sua porta
-                    </h3>
-                    <a href="orders" className="home-btn-order">FAZER PEDIDO</a>
+        <>
+            <nav className="home-container">
+                <div className="home-content">
+                    <div className="home-action">
+                        <h1 className="home-title">Faça seu pedido<br />que entregamos<br />pra você!!!</h1>
+                        <h3 className="home-subtitle">
+                            Escolha o seu pedido e em poucos minutos<br />levaremoss na sua porta
+                        </h3>
+                        <a href="orders" className="home-btn-order">FAZER PEDIDO</a>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
-                <div className="home-image">
-                    <MainImage />
-                </div>
-            </div>
-        </nav>
+            </nav>
+            <Footer />
+        </>
     )
 }
 
