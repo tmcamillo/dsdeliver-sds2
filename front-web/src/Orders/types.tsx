ebedad419a265149e486f6e20f12637f17af3618
Tatiane Mendes Camillo <tatiane@Alexs-MacBook-Pro.local>
2021-01-09 17:01:51 -0300

    search location add

--- front-web/src/Orders/types.tsx
+++ front-web/src/Orders/types.tsx
@@ -7,3 +7,9 @@ export type Product = {
 }
 
 //vantagem em usar typescript, pra ter segurança e ganho de produtividade por estar tipando o modelo de dados
+
+export type OrderLocationdata = {
+    latitude: number;
+    longitude: number;
+    address: string;
+}
